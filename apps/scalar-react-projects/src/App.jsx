9ba1a2d3e50470818b04af5d77714774a7f54926
Shubhraj Prasad Singh <ashu.shubhraj@gmail.com>
2024-08-21 18:26:53 +0530

    Added rfce component

--- apps/scalar-react-projects/src/App.jsx
+++ apps/scalar-react-projects/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
-
+import Bye from './components/Bye'
+import Hello from './components/Hello'
 function App() {
 
   return (
     <>
       <h1>Hello from Shubhraj</h1>
+      <Hello />
+      <Bye />
     </>
   )
 }
